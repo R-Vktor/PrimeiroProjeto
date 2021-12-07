200bbaef9b6574771c4f16e17334f38e5668730a
Victor alves <viktorw2@gmail.com>
2021-12-07 15:03:31 -0300

    Deletados arquivos e adicionado npm-shrink.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp        = require('gulp');
 const { watch, series, parallel, src, dest } = require('gulp'); // Dar o require nas seguintes dependencias torna a criacao das tasks menos verbosa
-const sass        = require('gulp-sass')(require('sass'));
+const sass        = require('gulp-sass');
 const browserSync = require('browser-sync').create(); // para criar servidor hot-reloader
 const plumber     = require('gulp-plumber');
 const uglify      = require('gulp-uglify');
@@ -29,22 +29,42 @@ const rename      = require('gulp-rename');
 let js_src   ="./assets/src/*.js";
 let sass_src ="./assets/src/*.scss";
 
+// Builded filed name
+let js_dist_name="script.js"
+let css_dist_name="style.css"
+
 // Dist Path
-let js_dist_name="scripts.js"
-let css_dist_name="scripts.css"
-let js_dist ="./assets/dist/";
+let js_dist ="./assets/dist";
+let css_dist ="./assets/dist";
+
 
 // Minify e Concat Scripts
-gulp.task('scripts', function() {
-    return src(js_src, sass_src)    // retorno da leitura dos respectivos arquivos
+gulp.task('js', function() {
+    return gulp.src(js_src)    // retorno da leitura dos respectivos arquivos
         .pipe(plumber())            //     
         .pipe(uglify())             // simplifica os codigos
         .pipe(concat(js_dist_name)) // junta tudo e coloca no arquivo designado
-        .pipe(dest(js_dist));       // pega o arquivo criado e coloca no diretorio designado
+        .pipe(gulp.dest(js_dist));       // pega o arquivo criado e coloca no diretorio designado
+});
+
+
+
+gulp.task('style', function() {
+    return src(sass_src)    // retorno da leitura dos respectivos arquivos
+        .pipe(plumber())
+        .pipe(sass())              //     
+        .pipe(uglify())               // simplifica os codigos
+        .pipe(concat(css_dist_name))  // junta tudo e coloca no arquivo designado
+        .pipe(dest(css_dist));        // pega o arquivo criado e coloca no diretorio designado
+        
 });
 
 
 // Os metodos 'series()' ou o 'parallel()' devem ser usados por determinacao da nova versao do gulp, podem ser colocados na task de watch ou na task principal.
 gulp.task('watch', function(){
-    watch([js_src], series(['scripts']));
+    watch([js_dist], series(['js']));
 });
+
+/*  
+ O metodo 'useref()' so concatena os arquivos JS ou CSS que forem colocados em nele separadamente e depois da transformacao junta todos, porem nao junta os arquivos soltos.
+*/
